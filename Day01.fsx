9a63256b809b6bc341db7507d1f4b5bdd491d44e
Valtteri Tarvus <valtteri.tarvus@gmail.com>
2022-12-01 09:59:59 +0200

    Rewrite

--- Day01.fsx
+++ Day01.fsx
@@ -1,20 +1,8 @@
-module Array =
-    let rec splitAtValue value input =
-        [|
-            match input |> Array.tryFindIndex((=) value) with
-            | Some index ->
-                let a, b = input |> Array.splitAt index
-                yield a
-                yield! splitAtValue value (b |> Array.tail)
-            | None -> ()
-        |]
+let input = System.IO.File.ReadAllText("inputs/01.txt")
 
-let input = System.IO.File.ReadAllLines("inputs/01.txt")
-
-let values = 
-    input
-    |> Array.splitAtValue ""
-    |> Array.map (Array.map int64 >> Array.sum)
+let values =
+    input.Split("\n\n")
+    |> Array.map (fun x -> x.Split("\n") |> Array.sumBy int64)
     |> Array.sortDescending
 
 values
