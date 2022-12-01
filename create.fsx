4ea2449e911187fa2bf82befbf5b87676a25ee41
Valtteri Tarvus <valtteri.tarvus@gmail.com>
2022-12-01 07:57:29 +0200

    Fix the script

--- create.fsx
+++ create.fsx
@@ -2,13 +2,13 @@
 open FsHttp
 let day = System.DateTime.Now.Day
 let filename = $"inputs/%02i{day}.txt"
-let url = $"https://adventofcode.com/2021/day/%i{day}/input"
+let url = $"https://adventofcode.com/2022/day/%i{day}/input"
 let session = fsi.CommandLineArgs.[1]
-printfn "%s" session
 
 // Write the template code reading the input
 let templateCode = $"let input = System.IO.File.ReadAllLines(\"{filename}\")"
 System.IO.File.WriteAllText($"Day%02i{day}.fsx", templateCode)
+
 // Get the input
 http {
     GET url
