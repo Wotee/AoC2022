af009257b9d9f69c990194b9aa42743afd50c83a
Valtteri Tarvus <valtteri6@msn.com>
2022-12-05 21:57:56 +0200

    Cleanup d5

--- Day05.fsx
+++ Day05.fsx
@@ -1,5 +1,4 @@
 open System
-open System.Collections.Generic
 let input = System.IO.File.ReadAllLines("inputs/05.txt")
 
 let parseMove (line : string) =
@@ -11,46 +10,26 @@ let moves, chart =
     |> Array.partition (fun x -> x.StartsWith("move"))
     |> fun (moves, chart) -> moves |> Array.map parseMove, chart |> Array.rev |> Array.skip 2
 
-let stackAmount = 
-    chart |> Array.head |> fun x -> (x.Length |> float) / 4. |> round |> int
-
-let lines =
+let values() =
     chart
-    |> Array.map(Seq.chunkBySize 4 >> Seq.mapi (fun i x -> match x |> Seq.item 1 with ' ' -> i, None | x -> i, Some x))
+    |> Array.map (Seq.chunkBySize 4 >> Seq.mapi (fun i x -> match x |> Seq.item 1 with ' ' -> None | x -> Some (i, x)) >> Seq.choose id)
+    |> Seq.collect id
+    |> Seq.groupBy fst
+    |> Seq.map (
+        snd
+        >> Seq.map snd
+        >> Seq.toList)
+    |> Seq.toArray
 
-let initStacks lines =
-    let stacks =
-        Array.init stackAmount (fun _ -> Stack<char>())
+let solve values order =
+    Array.fold (fun (acc : list<char> array) (a, f, t) ->
+        let move = acc[f][^(a-1)..] |> order
+        acc[f] <- acc[f][..^a]
+        acc[t] <-  acc[t]@move
+        acc
+    ) values
+    >> Array.map (List.last)
+    >> String.Concat
 
-    // Initialize stacks
-    lines
-    |> Array.fold (fun (current : Stack<char> array) line ->
-        line
-        |> Seq.iter (fun (index, value) ->
-            value
-            |> Option.iter (fun v -> current[index].Push(v))
-        )
-        current
-    ) stacks
-
-moves
-|> Array.fold (fun (current : Stack<char> array) (count, fromStack, toStack) ->
-    [|1..count|]
-    |> Array.iter (fun _ -> current[toStack].Push(current[fromStack].Pop()))
-    current
-) (initStacks lines)
-|> Array.map (fun stack -> stack.Pop())
-|> String.Concat
-|> printfn "Part 1: %s"
-
-moves
-|> Array.fold (fun (current : Stack<char> array) (count, fromStack, toStack) ->
-    [|1..count|]
-    |> Array.map (fun _ -> current[fromStack].Pop())
-    |> Array.rev
-    |> Array.iter (fun c -> current[toStack].Push(c))
-    current
-) (initStacks lines)
-|> Array.map (fun stack -> stack.Pop())
-|> String.Concat
-|> printfn "Part 2: %s"
+moves |> solve (values()) List.rev |> printfn "Part 1: %s"
+moves |> solve (values()) id |> printfn "Part 2: %s"
