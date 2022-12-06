ba572798856748f73db7dad1359716b8076bdbec
Valtteri Tarvus <valtteri6@msn.com>
2022-12-06 19:11:52 +0200

    Clean d6

--- Day06.fsx
+++ Day06.fsx
@@ -2,9 +2,8 @@ let input = System.IO.File.ReadAllText("inputs/06.txt")
 
 let solve length = 
     Seq.windowed length
-    >> Seq.mapi (fun i x -> i, x |> Set.ofSeq)
-    >> Seq.find (fun (_,x) -> x.Count = length)
-    >> fun (i,_) -> i + length
+    >> Seq.findIndex(fun x -> x |> Seq.distinct |> Seq.length = length)
+    >> (+) length
 
 input 
 |> solve 4
