9846e443f9139facec01591401e06411afd936b9
Valtteri Tarvus <valtteri.tarvus@gmail.com>
2022-12-09 13:47:04 +0200

    Cleanup

--- Day09.fsx
+++ Day09.fsx
@@ -22,18 +22,6 @@ let newTailPos x1 y1 x2 y2 =
 
 let newHeadPos x y = function | "R" -> x + 1, y | "L" -> x - 1, y | "U" -> x, y + 1 | "D" -> x, y - 1
 
-steps
-|> Seq.fold (fun (Hx, Hy, Tx, Ty, visited : Set<int*int>) elem ->
-    let newHx, newHy = newHeadPos Hx Hy elem
-    let newTx, newTy = newTailPos Tx Ty newHx newHy
-    newHx, newHy, newTx, newTy, (visited |> Set.add (newTx, newTy))
-) (0,0,0,0, Set.empty)
-|> fun (_, _, _, _, set) ->
-    printfn "Part 1: %i" set.Count
-
-let step2Tails = 
-    Array.init 9 (fun i -> i, (0,0)) |> Map.ofArray
-
 let rec updateTails n hx hy (tails : Map<int,int*int>) =
     if tails |> Seq.length = n then
         tails
@@ -42,11 +30,14 @@ let rec updateTails n hx hy (tails : Map<int,int*int>) =
         let headX, headY = newTails.Item n
         updateTails (n+1) headX headY newTails
 
-steps
-|> Seq.fold (fun (Hx, Hy, tails, visited : Set<int*int>) elem ->
-    let newHx, newHy = newHeadPos Hx Hy elem
-    let newTails = updateTails 0 newHx newHy tails
-    newHx, newHy, newTails, (visited |> Set.add newTails[8])
-) (0,0, step2Tails, Set.empty)
-|> fun (_, _, _, set) ->
-    printfn "Part2: %i" set.Count
+let solve amountOfTails steps =
+    steps
+    |> Seq.fold (fun (Hx, Hy, tails, visited : Set<int*int>) elem ->
+        let newHx, newHy = newHeadPos Hx Hy elem
+        let newTails = updateTails 0 newHx newHy tails
+        newHx, newHy, newTails, (visited |> Set.add newTails[amountOfTails - 1])
+    ) (0,0, (Array.init amountOfTails (fun i -> i, (0,0)) |> Map.ofArray), Set.empty)
+    |> fun (_, _, _, set) -> set.Count
+
+steps |> solve 1 |> printfn "Part 1: %i"
+steps |> solve 9 |> printfn "Part 2: %i"
