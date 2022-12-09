6985dc494f6d9ac49b03bb83a12b211ab620b101
Valtteri Tarvus <valtteri.tarvus@gmail.com>
2022-12-09 08:57:38 +0200

    Slight cleanup

--- Day09.fsx
+++ Day09.fsx
@@ -6,40 +6,30 @@ let steps =
     |> Array.map (fun (c, n) -> Seq.init n (fun _ -> c))
     |> Seq.collect id
 
-let newTailPos tailX tailY headX headY =
-    match tailX, tailY, headX, headY with
-    | x, y, nX, nY when x + 1 = nX && y = nY -> x, y
-    | x, y, nX, nY when x = nX && y + 1 = nY -> x, y
-    | x, y, nX, nY when x - 1 = nX && y = nY -> x, y
-    | x, y, nX, nY when x = nX && y - 1 = nY -> x, y
-    | x, y, nX, nY when x = nX && y < nY -> x, y + 1
-    | x, y, nX, nY when x = nX && y > nY -> x, y - 1
-    | x, y, nX, nY when y = nY && x < nX -> x + 1, y
-    | x, y, nX, nY when y = nY && x > nX -> x - 1, y
-    | x, y, nX, nY when y + 1 = nY && x + 1 = nX -> x, y
-    | x, y, nX, nY when y + 1 = nY && x - 1 = nX -> x, y
-    | x, y, nX, nY when y - 1 = nY && x + 1 = nX -> x, y
-    | x, y, nX, nY when y - 1 = nY && x - 1 = nX -> x, y
-    | x, y, nX, nY when y < nY && x < nX -> x + 1, y + 1
-    | x, y, nX, nY when y > nY && x < nX -> x + 1, y - 1
-    | x, y, nX, nY when y < nY && x > nX -> x - 1, y + 1
-    | x, y, nX, nY when y > nY && x > nX -> x - 1, y - 1
-    | x, y, nX, nY -> x, y
+let dist x1 y1 x2 y2 = 
+    System.Math.Sqrt((float x1 - float x2)**2.+(float y1 - float y2)**2.) |> int
 
-let newHeadPos Hx Hy = function
-    | "R" -> Hx + 1, Hy
-    | "L" -> Hx - 1, Hy
-    | "U" -> Hx, Hy + 1
-    | "D" -> Hx, Hy - 1
+let newTailPos x1 y1 x2 y2 = 
+    if dist x1 y1 x2 y2 > 1 then
+        let xDir = if x2 - x1 > 0 then (+) else (-)
+        let yDir = if y2 - y1 > 0 then (+) else (-)
+        match x1, y1, x2, y2 with
+        | xa, ya, xb, _ when xa = xb -> xa, yDir ya 1
+        | xa, ya, _, yb when ya = yb -> xDir xa 1, ya
+        | xa, ya, _, _ -> xDir xa 1, yDir ya 1
+    else
+        x1, y1
+
+let newHeadPos x y = function | "R" -> x + 1, y | "L" -> x - 1, y | "U" -> x, y + 1 | "D" -> x, y - 1
 
 steps
 |> Seq.fold (fun (Hx, Hy, Tx, Ty, visited : Set<int*int>) elem ->
-    let newHX, newHY = newHeadPos Hx Hy elem
-    let newTx, newTy = newTailPos Tx Ty newHX newHY
-    newHX, newHY, newTx, newTy, (visited |> Set.add (newTx, newTy))
+    let newHx, newHy = newHeadPos Hx Hy elem
+    let newTx, newTy = newTailPos Tx Ty newHx newHy
+    newHx, newHy, newTx, newTy, (visited |> Set.add (newTx, newTy))
 ) (0,0,0,0, Set.empty)
 |> fun (_, _, _, _, set) ->
-    printfn "%i" set.Count
+    printfn "Part 1: %i" set.Count
 
 let step2Tails = 
     Array.init 9 (fun i -> i, (0,0)) |> Map.ofArray
@@ -59,4 +49,4 @@ steps
     newHx, newHy, newTails, (visited |> Set.add newTails[8])
 ) (0,0, step2Tails, Set.empty)
 |> fun (_, _, _, set) ->
-    printfn "%i" set.Count
+    printfn "Part2: %i" set.Count
