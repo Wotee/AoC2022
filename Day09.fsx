83b0c3893eee6eb1582228a4fdfaa77c696187ae
Valtteri Tarvus <valtteri.tarvus@gmail.com>
2022-12-09 18:03:09 +0200

    Cleanup

--- Day09.fsx
+++ Day09.fsx
@@ -1,37 +1,26 @@
-let input = System.IO.File.ReadAllLines("inputs/09.txt")
-
 let steps =
-    input
-    |> Array.map (fun x -> x.Split(" ") |> fun y -> y[0], int y[1])
-    |> Array.map (fun (c, n) -> Seq.init n (fun _ -> c))
-    |> Seq.collect id
-
-let newTailPos x1 y1 x2 y2 =
-    let dist x1 y1 x2 y2 = System.Math.Sqrt((float x1 - float x2)**2.+(float y1 - float y2)**2.) |> int
-    let dir a b = match b - a with | 0 -> (*) | x when x < 0 -> (-) | _ -> (+)
-
-    if dist x1 y1 x2 y2 > 1 then
-        (dir x1 x2) x1 1, (dir y1 y2) y1 1
-    else
-        x1, y1
+    System.IO.File.ReadAllLines("inputs/09.txt")
+    |> Seq.collect (fun x -> x.Split(" ") |> fun y -> Seq.init (int y[1]) (fun _ -> y[0]))
 
 let newHeadPos x y = function | "R" -> x + 1, y | "L" -> x - 1, y | "U" -> x, y + 1 | "D" -> x, y - 1
 
-let rec updateTails n hx hy (tails : Map<int,int*int>) =
-    if tails |> Seq.length = n then
-        tails
-    else
-        let newTails = tails |> Map.change n (Option.map (fun (x, y) -> newTailPos x y hx hy))
-        let headX, headY = newTails.Item n
-        updateTails (n+1) headX headY newTails
+let newTail (x1, y1) (x2, y2) = 
+    let dir a b = match b - a with | 0 -> (*) | x when x < 0 -> (-) | _ -> (+)
+    match System.Math.Sqrt((float x1 - float x2)**2.+(float y1 - float y2)**2.) |> int with
+    | x when x > 1 -> (dir x1 x2) x1 1, (dir y1 y2) y1 1
+    | _ -> x1, y1
 
-let solve amountOfTails =
-    Seq.fold (fun (Hx, Hy, tails, visited : Set<int*int>) elem ->
-        let newHx, newHy = newHeadPos Hx Hy elem
-        let newTails = updateTails 0 newHx newHy tails
-        newHx, newHy, newTails, (visited |> Set.add newTails[amountOfTails - 1])
-    ) (0,0, (Array.init amountOfTails (fun i -> i, (0,0)) |> Map.ofArray), Set.empty)
-    >> fun (_, _, _, set) -> set.Count
+let update knots elem = 
+    let rec loop (tails : Map<int,int*int>) = function
+        | n when n = (tails |> Seq.length) -> tails
+        | n -> loop (tails |> Map.change n (Option.map (fun knot -> newTail knot tails[n-1]))) (n+1)
+    loop (knots |> Map.change 0 (Option.map (fun (x, y) -> newHeadPos x y elem))) 1
+
+let solve amountOfKnots = 
+    Seq.scan update (Array.init (amountOfKnots+1) (fun i -> i, (0,0)) |> Map.ofArray)
+    >> Seq.map (fun knots -> knots[amountOfKnots])
+    >> Set.ofSeq
+    >> fun set -> set.Count
 
 steps |> solve 1 |> printfn "Part 1: %i"
 steps |> solve 9 |> printfn "Part 2: %i"
