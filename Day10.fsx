9b3d1e482136bde4fba556d4746b221b91f971dd
Valtteri Tarvus <valtteri.tarvus@gmail.com>
2022-12-12 06:51:47 +0200

    Cleanup

--- Day10.fsx
+++ Day10.fsx
@@ -1,16 +1,15 @@
-open System
 let values =
-    IO.File.ReadAllLines("inputs/10.txt")
+    System.IO.File.ReadAllLines("inputs/10.txt")
     |> Array.collect (fun x -> x.Split(" "))
-    |> Array.scan (fun acc -> Int32.TryParse >> function | true, x -> acc + x | false, _ -> acc) 1
+    |> Array.scan (fun acc -> System.Int32.TryParse >> function | true, x -> acc + x | false, _ -> acc) 1
 
 let getVal (values : int array) i = values[i-1] * i
 
-[20;60;100;140;180;220]
+[20..40..220]
 |> List.sumBy (getVal values)
 |> printfn "Part 1: %i"
 
 values
 |> Array.chunkBySize 40
-|> Array.map (Array.mapi (fun i x -> if Math.Abs(i - x) <= 1 then "#" else " ") >> String.Concat)
+|> Array.map (Array.mapi (fun i x -> if System.Math.Abs(i - x) <= 1 then "#" else " ") >> System.String.Concat)
 |> fun res -> printfn "Part 2:"; res |> Array.iter (printfn "%s")
